2b9acfe65af3b3f1f87de5f7ec9b89dce4442036
samarhirau <samrhirau@gmail.com>
2024-07-08 09:41:04 +0530

    add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import { Routes } from './routes/Routes'
+import Footer from './component/Footer'
+import { Routes } from 'react-router-dom'
 
 function App() {
  
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         {}
       </Routes>
+      <Footer />
     </>
   )
 }
