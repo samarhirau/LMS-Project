35d6717102ecbf60dfa02455cb65055ef9706e81
samarhirau <samrhirau@gmail.com>
2024-07-29 13:08:50 +0530

    added routes for reset password

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,24 +1,27 @@
-import {Router} from 'express';
-import {getProfile, login, logout, register} from '../controllers/user.controller.js';
-import isLoggedIn from '../middlewares/auth.middleware.js';
-import upload from '../middlewares/multer.middleware.js';
-
-
-
-
-const router = Router();
-
-router.post('/register', (req, res) => {
-    res.send('register');
-});
-
-
-router.post('/register',upload.single("avatar"),register);
-router.post('/login',login);
-router.get('/logout',logout);
-router.get('/me',isLoggedIn,getProfile);
-
-
-
-
-export default router;
+// import { Router } from "express";
+import {
+    changePassword,
+    forgotPassword,
+    getLoggedInUserDetails,
+    loginUser,
+    logoutUser,
+    registerUser,
+    resetPassword,
+    updateUser,
+  } from "../controllers/user.controller.js";
+  import { isLoggedIn } from "../middlewares/auth.middleware.js";
+  import upload from "../middlewares/multer.middleware.js";
+  
+  const router = Router();
+  
+  router.post("/register", upload.single("avatar"), registerUser);
+  router.post("/login", loginUser);
+  router.post("/logout", logoutUser);
+  router.get("/me", isLoggedIn, getLoggedInUserDetails);
+  router.post("/reset", forgotPassword);
+  router.post("/reset/:resetToken", resetPassword);
+  router.post("/change-password", isLoggedIn, changePassword);
+  router.put("/update/:id", isLoggedIn, upload.single("avatar"), updateUser);
+  
+  export default router;
+  
--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ import cloudinary from 'cloudinary';
 // Start the server
 const PORT = process.env.PORT || 5000;
 
+
+// Cloudinary configuration
 cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
